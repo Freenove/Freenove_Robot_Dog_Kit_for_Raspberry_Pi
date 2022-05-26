caea04f1448e3206af5fe69a62d1e926b9e71e91
DenzelChen <291689742@qq.com>
2022-05-26 15:10:35 +0800

    Update setup_windows.py

--- Code/setup_windows.py
+++ Code/setup_windows.py
@@ -11,23 +11,19 @@ for x in range(1,4):
 		flag=flag | 0x02
 		break
 for x in range(1,4):
-	if os.system("pip3 install pyqt5-tools") == 0:
-		flag=flag | 0x04
-		break
-for x in range(1,4):
 	if os.system("pip3 install Pillow") == 0:
-		flag=flag | 0x08
+		flag=flag | 0x04
 		break
 for x in range(1,4):
 	if os.system("pip3 install opencv-python") == 0:
 		if os.system("pip3 install opencv-contrib-python") == 0:
-			flag=flag | 0x10
+			flag=flag | 0x08
 			break
 for x in range(1,4):
 	if os.system("pip3 install numpy") == 0:
-		flag=flag | 0x20
+		flag=flag | 0x10
 		break
-if flag==0x3f:
+if flag==0x1f:
 		os.system("pip3 list && pause")
 		print("\nAll libraries installed successfully")
 else:
